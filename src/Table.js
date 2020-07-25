f8e905036cddcbfcc8fca1b597f31ae5d04af55c
Jameem <jameemkurikkal.mp@gmail.com>
2020-07-25 20:22:37 +0530

    table update

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,5 @@
 import React from "react"
+import numeral from "numeral"
 import "./Table.css"
 
 function Table({ countries }) {
@@ -7,7 +8,10 @@ function Table({ countries }) {
       {countries.map(({ country, cases }) => (
         <tr>
           <td>{country}</td>
-          <td>{cases}</td>
+          <td>
+            {" "}
+            <strong>{numeral(cases).format("0,0")}</strong>
+          </td>
         </tr>
       ))}
     </div>
